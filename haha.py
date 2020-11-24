acf7dba7efd7bc3dfe62c7adc3e915d6bf07f64b
xyq <1355226831@qq.com>
2020-11-24 21:38:13 +0800

    commit haha

--- haha.py
+++ haha.py
@@ -2,5 +2,11 @@ a = 1
 #modified
 b = 2
 #committed but i want to do some change and dont change the graph
+<<<<<<< Updated upstream
 #commit from boss require
+=======
+#i am at dev branch and commit in dev
+#i am in dev coding
+
+>>>>>>> Stashed changes
 
